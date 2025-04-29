c6a8b66506c57c28043b5a5347bc2dc03197fe96
Rob Knop <raknop@lbl.gov>
2025-04-29 11:29:09 -0700

    Fix run_postgres.sh to work

--- docker/postgres/run_postgres.sh
+++ docker/postgres/run_postgres.sh
@@ -17,23 +17,27 @@ if [[ x"$PGPASS" == "x" ]]; then
     PGPASS="fragile"
 fi
 
+if [[ x"$PGADMINPASS" == "x" ]]; then
+    PGADMINPASS="fragile"
+fi
+
 if [[ x"$PGNAME" == "x" ]]; then
     PGNAME="unknown_database"
 fi
 
 if [ ! -f $POSTGRES_DATA_DIR/PG_VERSION ]; then
     echo "Running initdb in $POSTGRES_DATA_DIR"
-    echo $PGPASS > $HOME/pwfile
+    echo $PGADMINPASS > $HOME/pwfile
     /usr/lib/postgresql/15/bin/initdb -U postgres --pwfile=$HOME/pwfile $POSTGRES_DATA_DIR
     rm $HOME/pwfile
     /usr/lib/postgresql/15/bin/pg_ctl -D $POSTGRES_DATA_DIR start
     if [[ $PGUSER != "postgres" ]]; then
         echo "Creating user $PGUSER"
-        psql --comand "CREATE USER $PGUSER PASSWORD '$PGPASS' CREATEDB"
+        psql -U postgres --command "CREATE USER $PGUSER PASSWORD '$PGPASS' CREATEDB"
     fi
     echo "Creating database $PGNAME"
-    psql --command "CREATE DATABASE $PGNAME OWNER $PGUSER"
-    psql --command "CREATE EXTENSION q3c" $PGNAME
+    psql -U postgres --command "CREATE DATABASE $PGNAME OWNER $PGUSER"
+    psql -U postgres --command "CREATE EXTENSION q3c" $PGNAME
     /usr/lib/postgresql/15/bin/pg_ctl -D $POSTGRES_DATA_DIR stop
 fi
 exec /usr/lib/postgresql/15/bin/postgres -c config_file=/etc/postgresql/15/main/postgresql.conf
